1d0cd713ba67755739375520e6096a4ac3bc6376
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-03-24 20:03:28 +0000

    added procfile

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
 const server = require("./server.js")
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`Express departing now from http://localhost:${port}`));
